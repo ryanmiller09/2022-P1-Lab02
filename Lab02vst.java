aedb5210e56fa347e10d2fc4116cb015eae1d068
13195 <ryan.miller@vtsd.com>
2022-09-14 09:26:52 -0400

    gf

--- Lab02vst.java
+++ Lab02vst.java
@@ -10,5 +10,14 @@
 
     public static void main(String args[]){
 
+      System.out.println("Lab 02, 80 Point Version");
+      System.out.println();
+      System.out.println();
+      System.out.println("Measure today's knowledge ");
+      System.out.println("by yesterday's confusion.");
+      System.out.println();
+      System.out.println("Bewilderment + Exposure = Obvious");
+      System.out.println();
+
     }
   }
